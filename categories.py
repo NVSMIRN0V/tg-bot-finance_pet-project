1c90f66d41d8792c68481128cad88a5a019db704
Nikita Smirnov <job.nvsmirnov@gmail.com>
2024-05-08 12:12:31 +0300

    fetchall implemented
    Реализовал метод fetchall для удобного взаимодействия с бд.
    Добавил новый класс Categories, чтобы хранить их в отдельном классе и меньше обращаться к бд.

--- categories.py
+++ categories.py
@@ -8,3 +8,16 @@ class Category:
     name: str
     altname: str
     aliases: list[str]
+
+
+class Categories:
+    def __init__(self) -> None:
+        self._categories = self._load_categories()
+
+    def _load_categories(self) -> list[Category]:
+        '''Загрузить все категории из бд'''
+        pass
+
+    def get_category(self) -> Category:
+        '''Получить категорию по ее имени или алиасу'''
+        pass
--- sql.py
+++ sql.py
@@ -7,6 +7,8 @@ cursor = connection.cursor()
 
 
 def insert(table: str, data: dict) -> None:
+    '''Запись в таблицу новых данных'''
+
     # Парсинг data
     values = tuple(data.values())
     columns = ', '.join(data.keys())
@@ -17,3 +19,23 @@ def insert(table: str, data: dict) -> None:
         f'values {values}'
     )
     connection.commit()
+
+
+def fetchall(table: str, cols: list[str]) -> list[dict]:
+    '''Получение результата запроса в бд'''
+
+    # Чтение записей из бд
+    cols_joined = ', '.join(cols)
+    cursor.execute(
+        f'select {cols_joined} from {table}'
+    )
+    objects = cursor.fetchall()
+
+    # Распаковка записей в нужный формат
+    unpacked_objects = []
+    for obj in objects:
+        dict_obj = {}
+        for index, col in enumerate(cols):
+            dict_obj[col] = obj[index] 
+        unpacked_objects.append(dict_obj)        
+    return unpacked_objects
